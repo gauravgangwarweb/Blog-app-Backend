18695a197701051f9e8b97e9de51a4857e4dbdc7
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-07-10 18:35:46 +0530

    Add user model file with user schema

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import Post from "./postModel";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        require: true,
+        type: String
+    },
+    lastName: {
+        require: true,
+        type: String
+    },
+    email: {
+        require: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        require: true,
+        type: String
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+userSchema.virtual('posts', {
+    ref: 'Post',
+    localField: '_id',
+    foreignField: 'userId',
+});
+
+const User = mongoose.model('User', userSchema)
+
+export default User
