b374a1a13900490c6a30dc305452216583eda16f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-07-10 18:38:13 +0530

    Add post route

---
+++ routes/postRoute.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { createPost, getAllPost, getPostById, updatePost, deletePost } from '../controllers/postController'
+
+const router = express.Router()
+
+router.post('/post/new', createPost)
+
+router.get('/posts', getAllPost)
+
+router.get('/post/:id', getPostById)
+
+router.put('/post/:id', updatePost)
+
+router.delete('/post/:id', deletePost)
+
+export default router
